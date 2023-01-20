54d974275c20b0225543fd6fe08822862a9f4219
BakuDoriya-KatsZuku <96240951+BakuDoriya-KatsZuku@users.noreply.github.com>
2023-01-20 21:49:06 +0530

    Hao

--- Binary Search Tree, Inorder, Preorder, Postorder.c
+++ Binary Search Tree, Inorder, Preorder, Postorder.c
@@ -1,81 +1,77 @@
 #include <stdio.h>
 
-// Defining Data Structure
-typedef struct
+typedef struct 
 {
 	int data;
-	struct node *left;
-	struct node *right;
+	struct node* left;
+	struct node* right;
 }node;
 
-//Prototyping
-node *insert(node *root, int n);
-void inorder(node *root);
-void preorder(node *root);
-void postorder(node *root);
-node *delete(node *root, int n);
-void search(node *root, int n);
+node* insert(node* root, int n);
+void preorder(node* root);
+void inorder(node* root);
+void postorder(node* root);
+void search(node* root, int n);
+node* delete(node* root, int n);
 
 int main(void)
 {
-	node *root = NULL;
+	node* root = NULL;
 	int n = 0;
-	printf("Number of elements: ");
 	scanf("%i", &n);
 	int x = 0;
 	for (int i = 0; i < n; i++)
 	{
-		printf("Data: ");
 		scanf("%i", &x);
-		root = insert(root, x);	
+		root = insert(root, x);
 	}
 
-	printf("\nInorder: ");
-	inorder(root);
-	printf("\nPreorder: ");
+	printf("Preorder: ");
 	preorder(root);
-	printf("\nPostorder: ");
+	printf("\n");
+	printf("Inorder: ");
+	inorder(root);
+	printf("\n");
+	printf("Postorder: ");
+	postorder(root);
+	printf("\n");
+	search(root, 69);
+	delete(root, 69);
+	delete(root, 100);
+	delete(root, 1);
+	printf("Preorder: ");
+	preorder(root);
+	printf("\n");
+	printf("Inorder: ");
+	inorder(root);
+	printf("\n");
+	printf("Postorder: ");
 	postorder(root);
 }
 
-node *insert(node *root, int n)
+node* insert(node* root, int n)
 {
-	//If node is not present make it else go ahead for finding the position of insertion
 	if (root == NULL)
 	{
 		root = (node*)malloc(sizeof(node));
-		root->left = NULL;
-		root->right = NULL;
 		root->data = n;
+		root->right = NULL;
+		root->left = NULL;
 		return (root);
+	}
+	if (n > root->data)
+	{
+		root->right = insert(root->right, n);
 	}
 	else
 	{
-		if (n < root->data)
-		{
-			root->left = insert(root->left, n);
-		}
-		else
-		{
-			root->right = insert(root->right, n);
-		}
+		root->left = insert(root->left, n);
 	}
 
 	return (root);
 }
 
-void inorder(node *root)
-{
-	if (root == NULL)
-	{
-		return;
-	}
-	inorder(root->left);
-	printf("%i ", root->data);
-	inorder(root->right);
-}
-
-void preorder(node *root)
+void preorder(node* root)
 {
 	if (root == NULL)
 	{
@@ -86,7 +82,18 @@ void preorder(node *root)
 	preorder(root->right);
 }
 
-void postorder(node *root)
+void inorder(node* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	inorder(root->left);
+	printf("%i ", root->data);
+	inorder(root->right);
+}
+
+void postorder(node* root)
 {
 	if (root == NULL)
 	{
@@ -97,115 +104,66 @@ void postorder(node *root)
 	printf("%i ", root->data);
 }
 
-node *delete(node *root, int n)
-{
-	node *temp = root;
-	node *temp2 = root;
-	while (temp->data != n)
-	{
-		if (n < temp->data)
-		{
-			temp2 = temp;
-			temp = temp->left;
-		}
-		else
-		{
-			temp2 = temp;
-			temp = temp->right;
-		}
-	}
-
-	if (temp->left == NULL && temp->right == NULL)
-	{
-		if (temp2->left == temp)
-		{
-			temp2->left = NULL;
-		}
-		else
-		{
-			temp2->right = NULL;
-		}
-	}
-	else if ((temp->left == NULL || temp->right == NULL))
-	{
-		if (temp->right == NULL)
-		{
-			if (temp2->left == temp)
-			{
-				temp2->left = temp->left;
-			}
-			else if (temp2 == temp)
-			{
-				root = temp->left;
-			}
-			else
-			{
-				temp2->right = temp->left;
-			}
-		}
-		else
-		{
-			if (temp2->left == temp)
-			{
-				temp2->left = temp->right;
-			}
-			else if (temp == temp2)
-			{
-				root = temp->right;
-			}
-			else
-			{
-				temp2->right = temp->right;
-			}
-		}
-	}
-	else
-	{
-		if (temp2->left == temp)
-		{
-			temp2->left = temp->right;
-		}
-		else if (temp2 == temp)
-		{
-			root = temp->right;
-		}
-		else
-		{
-			temp2->right = temp->right;
-		}
-		node *t = temp->right;
-		while (t->left != NULL)
-		{
-			t = t->left;
-		}
-		t->left = temp->left;
-	}
-
-	temp->right = NULL;
-	temp->left = NULL;
-	free(temp);
-
-	return (root);
-}
-
-void search(node *root, int n)
+void search(node* root, int n)
 {
 	if (root == NULL)
 	{
-		printf("Not Found");
+		printf("Not Found.\n");
 		return;
 	}
-	if (n == root->data)
+	if (root->data == n)
 	{
-		printf("Found");
+		printf("Found.\n");
 		return;
 	}
-	else if (n < root->data)
-	{
-		search(root->left, n);
-	}
-	else
+	else if (n > root->data)
 	{
 		search(root->right, n);
 	}
+	else
+	{
+		search(root->left, n);
+	}
+}
+
+node* delete(node* root, int n)
+{
+	if (root == NULL)
+	{
+		return (root);
+	}
+
+	if (n > root->data)
+	{
+		root->right = delete(root->right, n);
+	}
+	else if (n < root->data)
+	{
+		root->left = delete(root->left, n);
+	}
+	else
+	{
+		if (root->left == NULL)
+		{
+			node* p = root->right;
+			free(root);
+			return (p);
+		}
+		else if (root->right == NULL)
+		{
+			node* p = root->left;
+			free(root);
+			return (p);
+		}
+
+		node* temp = root->right;
+		while (temp->left != NULL)
+		{
+			temp = temp->left;
+		}
+		root->data = temp->data;
+
+		root->right = delete(root->right, temp->data);
+	}
+	return (root);
 }
